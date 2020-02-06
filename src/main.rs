d71df0ae5727e8eb6d84ca7ef90131fd37e40325
Jacob Davis-Hansson <jakewins@gmail.com>
2020-02-05 19:11:53 -0600

    beginnings

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+extern crate clap;
+
+use clap::{Arg, App, SubCommand, AppSettings};
+
+use pest::Parser;
+use std::fs;
+
+#[derive(Parser)]
+#[grammar = "cypher.pest"]
+pub struct CypherParser;
+
+fn main() {
+    let matches = App::new("graf")
+        .version("0.0")
+        .author("Jacob Davis-Hansson <jake@davis-hansson.com>")
+        .about("A graph database in a yaml file!")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .args_from_usage(
+            "-f, --file=[FILE] @graf.yaml 'Sets the db file to use'\
+            -h, --help 'Print help information'")
+        .subcommand(SubCommand::with_name("query")
+            .about("executes a query against the database")
+            .arg_from_usage(
+                "<QUERY> 'Query to execute'"))
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("query") {
+        let query_str = matches.value_of("QUERY").unwrap();
+
+        let query = CypherParser::parse(Rule::query, &query_str)
+            .expect("unsuccessful parse") // unwrap the parse result
+            .next().unwrap(); // get and unwrap the `file` rule; never fails
+
+        let mut statement_count: u64 = 0;
+
+        for statement in query.into_inner() {
+            match statement.as_rule() {
+                Rule::statement => {
+                    statement_count += 1;
+                }
+                Rule::EOI => (),
+                _ => unreachable!(),
+            }
+        }
+
+        println!("Number of statements: {}", statement_count);
+    }
+}
