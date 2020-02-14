86f160f2eb67780bc37886093e00a5be50a6349f
Jacob Davis-Hansson <jakewins@gmail.com>
2020-02-14 13:51:17 +0100

    Explain why we leak memory on every query

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,6 @@ use std::rc::Rc;
 use std::fmt::{Display, Formatter, Write};
 use std::fmt;
 
-// lol
-fn string_to_static_str(s: &str) -> &'static str {
-    Box::leak(s.to_string().into_boxed_str())
-}
-
 fn main() -> Result<(), Error>{
     let matches = App::new("g")
         .version("0.0")
@@ -38,4 +33,14 @@ fn main() -> Result<(), Error>{
 
     }
     Ok(())
+}
+
+
+// TODO: The reason for this is that the cursor borrows part of the query string when you
+//       run a query, and rather than deal with setting proper lifetimes for that I think we can
+//       get rid of that memory sharing entirely, maybe? Although maybe the borrow is actually
+//       kind of sensible; it'd mean queries with large string properties don't need to copy those
+//       strings in, for instance..
+fn string_to_static_str(s: &str) -> &'static str {
+    Box::leak(s.to_string().into_boxed_str())
 }
