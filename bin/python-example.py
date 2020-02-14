966769dbba1ac37f2529c959f0b74985ecbc627c
Jacob Davis-Hansson <jakewins@gmail.com>
2020-02-14 01:17:28 +0100

    Restructure capi to start becoming usable

--- bin/python-example.py
+++ bin/python-example.py
@@ -13,7 +13,10 @@ else:
 
 lib = cdll.LoadLibrary('/home/jake/Code/toy/graf/target/debug/{}gqlite.{}'.format(prefix, ext))
 
-cursor = lib.gqlite_run(c_char_p("MATCH (n:Note) RETURN n.message".encode('utf-8')))
+db = lib.gqlite_open(c_char_p("gram://miserables.gram".encode('utf-8')))
+cursor = lib.gqlite_cursor_new(db)
+
+res = lib.gqlite_run(cursor, c_char_p("MATCH (n:Note) RETURN n.message".encode('utf-8')))
 print(f"{cursor}")
 
 res = lib.gqlite_cursor_next(cursor)
--- gqlite-capi/src/lib.rs
+++ gqlite-capi/src/lib.rs
@@ -3,23 +3,48 @@ use std::ffi::CStr;
 use std::os::raw::{c_char, c_int};
 use std::mem::transmute;
 
+
 #[repr(C)]
 #[derive(Debug)]
-pub struct Cursor {
+pub struct database {
     pub msg : c_int,
 }
 
+#[repr(C)]
+#[derive(Debug)]
+pub struct cursor {
+    pub msg : c_int,
+}
+
+
+// Open a database file
 #[no_mangle]
-pub extern fn gqlite_run(raw_query: *const c_char) -> *mut Cursor {
+pub extern fn gqlite_open(raw_url: *const c_char) -> *mut database {
+    let url_bytes = unsafe { CStr::from_ptr(raw_url).to_bytes() };
+    let url: &str = str::from_utf8(url_bytes).unwrap();
+    println!("opening {}", url);
+    return unsafe { transmute(Box::new(database { msg: 1337, })) }
+}
+
+// Allocate a new cursor for the specified database
+#[no_mangle]
+pub extern fn gqlite_cursor_new(db: *mut database) -> *mut cursor {
+    return unsafe { transmute(Box::new(cursor { msg: 1337, })) }
+}
+
+// Run a query, optionally provide result in cursor
+#[no_mangle]
+pub extern fn gqlite_run(raw_cursor: *mut cursor, raw_query: *const c_char) -> c_int {
     println!("You are in gqlite");
     let query_bytes = unsafe { CStr::from_ptr(raw_query).to_bytes() };
     let query: &str = str::from_utf8(query_bytes).unwrap();
     println!("{}", query);
-    return unsafe { transmute(Box::new(Cursor { msg: 1337, })) }
+    return 0
 }
 
+// Move cursor to next row
 #[no_mangle]
-pub extern fn gqlite_cursor_next(raw_cursor: *mut Cursor) -> c_int {
+pub extern fn gqlite_cursor_next(raw_cursor: *mut cursor) -> c_int {
     let cursor = unsafe { &mut *raw_cursor };
     println!("{:?}.next", cursor.msg);
     return 0
